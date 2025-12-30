126ffd0a5d6964fa15915808097623ac52245b07
alexsnow <alexsandr.styazhkov@gmail.com>
2025-12-30 13:13:41 +0500

    Add Astro deployment workflow

--- callous-chaos/astro.config.mjs
+++ callous-chaos/astro.config.mjs
@@ -1,5 +1,9 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  // Указываем ваш новый домен
+  site: 'https://snowsolutions.github.io',
+  // Если репозиторий называется snowsolutions.github.com, 
+  // то GitHub обычно разворачивает его в корень.
+  base: '/', 
+});
