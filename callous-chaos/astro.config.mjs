a57092359c59cf52650e5495551b6172ef6115b5
alexsnow <alexsandr.styazhkov@gmail.com>
2025-12-30 13:22:11 +0500

    Fix repository name in config

--- callous-chaos/astro.config.mjs
+++ callous-chaos/astro.config.mjs
@@ -1,9 +1,6 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  // Указываем ваш новый домен
   site: 'https://snowsolutions.github.io',
-  // Если репозиторий называется snowsolutions.github.com, 
-  // то GitHub обычно разворачивает его в корень.
   base: '/', 
 });
