657f7ea5b59d8f4937d4242c7663185bcfaf8d28
alexsnow <alexsandr.styazhkov@gmail.com>
2026-01-04 23:33:11 +0500

    refresh index page

--- README.md
+++
@@ -1,2 +0,0 @@
-# snowsolutions.github.com
-# styazhkov.github.io
--- callous-chaos/astro.config.mjs
+++
@@ -1,6 +0,0 @@
-import { defineConfig } from 'astro/config';
-
-export default defineConfig({
-  site: 'https://snowsolutions.github.io',
-  base: '/', 
-});
